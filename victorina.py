34c743636b6b033fe1d95621a8540f2c2cf7ce7f
KarToveD <67981770+Kartoved24@users.noreply.github.com>
2023-03-07 19:53:22 +0300

    Добавили красоты, вывод баллов

--- victorina.py
+++ victorina.py
@@ -1,6 +1,15 @@
 import json
 import random
 
+ball_1 = 0
+ball_2 = 0
+lifes = 0
+lifes_1 = 1
+lifes_2 = 1
+
+def say_hi():
+    print('Добро пожаловать в викторину!')
+
 def import_questions_base():
     global questions_base
     with open('questions.json', encoding='UTF=8') as file:
@@ -8,29 +17,53 @@ def import_questions_base():
 
 
 def set_lifes():
+    global lifes
+    global lifes_1
+    global lifes_2
     try:
-        lifes = int(input('Введите количество жизней '))
+        lifes = int(input('Введите количество жизней больше нуля '))
+        lifes_1 = lifes
+        lifes_2 = lifes
     except:
         print('аяяй! введите число!!')
-    return lifes
 
 def add_players():
-    player_1 = input('Введите имя первого игрока')
-    player_2 = input('Введите имя второго игрока')
-    return player_1, player_2
+    global player_1
+    global player_2
+    player_1 = input('Введите имя первого игрока ')
+    player_2 = input('Введите имя второго игрока ')
+
   
 def choose_random_question():
+    """выдает случайный вопрос из базы"""
+    global lifes
+    global lifes_1
+    global lifes_2
+    global ball_1
+    global ball_2
     question = random.choice(questions_base)
+    questions_base.remove(question)
     for key in question.keys():
         print(key)
         break
-    answer = input('').lower()
-    if answer == question[key]:
-        print('ты угадал')
+    print(f'Ходит {player_1.title()}')
+    if input('\t').lower() == question[key]:
+        ball_1 += 1
+        print(f'ты угадал. У тебя {ball_1} баллов')
+    else:
+        lifes_1 -= 1
+        print(f'Ты не угадал. Теряешь жизнь! Теперь у тебя {lifes_1} жизней. У тебя {ball_1} баллов\n')
+    print(f'Ходит {player_2.title()}')
+    if input('\t').lower() == question[key]:
+        ball_2 += 1
+        print(f'ты угадал. У тебя {ball_2} баллов')
+    else:
+        lifes_2 -= 1
+        print(f'Ты не угадал. Теряешь жизнь! Теперь у тебя {lifes_2} жизней. У тебя {ball_2} баллов\n')
 
-import_questions_base()
-choose_random_question() 
-# while True:
-#     set_lifes()
-#     add_players()
+set_lifes()
+add_players()
+while lifes_1 > 0 or lifes_2 > 0: 
+    import_questions_base()
+    choose_random_question()
 
